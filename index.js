67d053054369fb42cd81179da32938837afaa1d0
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-14 23:19:41 -0600

    added figlet and connection to the db function

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // create the dependencies
-const mySql = require('mysql2')
-const inquirer = require('inquirer')
-const consoleTable = require("console.table")
-const figlet = require('figlet')
+const mySql = require("mysql2");
+const inquire = require("inquire");
+const consoleTable = require("console.table");
+const figlet = require("figlet");
 
+//figlet module prompt
+figlet("EMPLOYEE TRACKER", function (err, data) {
+  if (err) {
+    console.log("Please restart the app!");
+  }
+  console.log(data);
+});
 
+// Connect to the database using dbnode i
+const dbConnection = mySql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "employeeDb",
+});
