a73ec89377873edefbef672adb524ccf130c5763
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-14 21:14:33 -0600

    added index file to the main folder

---
+++ index.js
@@ -0,0 +1,7 @@
+// create the dependencies
+const mySql = require('mysql2')
+const inquirer = require('inquirer')
+const consoleTable = require("console.table")
+const figlet = require('figlet')
+
+
