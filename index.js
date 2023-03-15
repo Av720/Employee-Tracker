b5494ac80a387e891e0dcbcc4bd74ade4a312e65
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-14 23:55:07 -0600

    added firstPrompt questions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // create the dependencies
 const mySql = require("mysql2");
-const inquire = require("inquire");
+const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const figlet = require("figlet");
 
@@ -19,3 +19,42 @@ const dbConnection = mySql.createConnection({
   password: "root",
   database: "employeeDb",
 });
+
+dbConnection.connect(err =>  {
+    if (err) throw err;
+    
+    //log the connection using thread id 
+    console.log(`Successfully connected as id ${dbConnection.threadId}!`);
+
+    // this will start the first prompt and execute the function 
+    firstPrompt();
+})
+
+// firstPrompt function - this will prompt the user with the choices to execute
+function firstPrompt() {
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'View all employees',
+                'View all employees by department',
+                'View all employees by Manager',
+                'Add employee',
+                'Remove employee',
+                'Update employee role',
+                'Update employee manager',
+                'View all roles',
+                'Add role',
+                'Remove role',
+                'View all departments',
+                'Add a department',
+                'Remove department',
+                'View department budgets',
+                'Quit']
+        }
+
+    ])
+} // firstPrompt end 
