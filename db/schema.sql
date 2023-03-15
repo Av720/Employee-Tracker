e430af1c1d59582d60374385bdff37e5aefb71c1
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-14 22:01:40 -0600

    decimal for the salary)

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,7 @@ name VARCHAR(30) NOT NULL
 CREATE TABLE role (
 id INT PRIMARY KEY AUTO_INCREMENT,
 title VARCHAR(30) NOT NULL, 
-salary DECIMAL NOT NULL,
+salary DECIMAL (10,2) NOT NULL,
 department_id INT,
 FOREIGN KEY (department_id) REFERENCES department(id)
 
