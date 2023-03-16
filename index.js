517b1058c641a09403de6ba452bd3715f0485d3c
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-15 21:02:23 -0600

    added functions to view the tables from workbench

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mySql = require("mysql2");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const figlet = require("figlet");
+const { first } = require("rxjs");
 const log = console.log;
 const tableLog = console.table;
 
@@ -57,33 +58,198 @@ function firstPrompt() {
           "View department budgets",
           "Quit",
         ],
-      },\playlist\4EjIhZWbxJNNiHPwVh17mF
-    ]) 
-      // switch/case statement for the user input
+      },
+    ])
+    // switch/case statement for the user input
     .then(function (val) {
       switch (val.choices) {
+        //"View all employees", user input
         case "View all employees":
           viewAllEmployees();
           break;
+
+        // "View all employees by department",user input
+        case "View all employees by department":
+          employeesByDept();
+          break;
+
+        // "View all employees by Manager", user input
+        case "View all employees by Manager":
+          employeesByMngr();
+          break;
+
+        //"Add employee", user input
+        case "Add employee":
+          addEmployee();
+          break;
+
+        // "Remove employee", user input
+        case "Remove employee":
+          removeEmployee();
+          break;
+
+        //   "Update employee role", user input
+        case "Update employee role":
+          updateEmployee();
+          break;
+
+        // "Update employee role", user input
+        case "Update employee role":
+          updateEmploRole();
+          break;
+
+        //"Update employee manager", user input
+        case "Update employee manager":
+          updateEmploMngr();
+          break;
+
+        // "View all roles", user input
+        case "View all roles":
+          viewAllRoles();
+          break;
+
+        //"Add role", user input
+        case "Add role":
+          addRole();
+
+        // "Remove role", user input
+        case "Remove role":
+          removeRole();
+          break;
+
+        //"View all Departments", user input
+        case "View all departments":
+          viewAllDepartments();
+          break;
+
+        //"Add a department", user input
+        case "Add a department":
+          addDept();
+          break;
+
+        //"Remove Department", user input
+        case "Remove department":
+          removeDept();
+          break;
+
+        // "View department budgets"
+        case "View department budgets":
+          deptBudgets();
+          break;
+
+        // "Quit the app"
+        case "Quit":
+          quit();
       }
-    });
-  // view all employees function
-  function viewAllEmployees() {
-    const query = "SELECT * FROM employee";
-    dbConnection.query(query, (err, res) => {
-      if (err) throw err;
-      tableLog(res);
 
-      firstPrompt();
-    });
-    }
-    
-    // view all employees by department function
+      //-------------------------------------------------------------------------//
+      // view all employees function
+      function viewAllEmployees() {
+        const query = "SELECT * FROM employee";
+        dbConnection.query(query, (err, res) => {
+          if (err) throw err;
+          tableLog(res);
 
-    
+          firstPrompt();
+        });
+      }
+      //-------------------------------------------------------------------------//
 
+      // view all employees by department function
+      function employeesByDept() {
+        const query = `SELECT employee.first_name, 
+            employee.last_name,
+                department.name AS department
+               FROM employee 
+               LEFT JOIN role ON employee.role_id = role.id 
+               LEFT JOIN department ON role.department_id = department.id`;
+        dbConnection.query(query, (err, res) => {
+          if (err) throw err;
+          tableLog(res);
 
-}
+          firstPrompt();
+        });
+      }
+      //-------------------------------------------------------------------------//
+      function employeesByMngr() {
+        const query = "";
+      }
+      //-------------------------------------------------------------------------//
+      function addEmployee() {
+        const query = "";
+      }
+      //-------------------------------------------------------------------------//
+      function removeEmployee() {
+        const query = "";
+      }
+
+      //-------------------------------------------------------------------------//
+      function updateEmployee() {
+        const query = "";
+      }
+      //-------------------------------------------------------------------------//
+      function updateEmploRole() {
+        const query = "";
+      }
+      //-------------------------------------------------------------------------//
+      function updateEmploMngr() {
+        const query = "";
+      }
+
+      //-------------------------------------------------------------------------//
+      function viewAllRoles() {
+        const query = `SELECT role.id, role.title, department.name AS department
+               FROM role
+               INNER JOIN department ON role.department_id = department.id`;
+        dbConnection.query(query, (err, res) => {
+          if (err) throw err;
+          tableLog(res);
+
+          firstPrompt();
+        });
+      }
+      //-------------------------------------------------------------------------//
+      function addRole() {
+        const query = "";
+      }
+      //-------------------------------------------------------------------------//
+      function removeRole() {
+        const query = "";
+      }
+      //-------------------------------------------------------------------------//
+      function viewAllDepartments() {
+          const query = 'SELECT * FROM department';
+        dbConnection.query(query, (err, res) => {
+          if (err) throw err;
+          tableLog(res);
+
+          firstPrompt();
+        });
+      }
+      //-------------------------------------------------------------------------//
+      function addDept() {
+        const query = "";
+      }
+      //-------------------------------------------------------------------------//
+      function removeDept() {
+        const query = "";
+      }
+      //-------------------------------------------------------------------------//
+      function removeDept() {
+        const query = "";
+      }
+      //-------------------------------------------------------------------------//
+      function deptBudgets() {
+        const query = "";
+      }
+      //-------------------------------------------------------------------------//
+      function quit() {
+        console.log("Have a good day! ");
+        process.exit();
+      }
+      //-------------------------------------------------------------------------//
+    }); // end of .then function
+} // end of first prompt function
 
 // firstPrompt end
 firstPrompt();
